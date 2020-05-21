99a31a8bb6ff7bdd8736a0dfc31a0893cb28f8e6
unknown <swamy.dammala13@gmail.com>
2020-05-21 18:31:08 +0530

    Set up formatting for amount and date

--- src/Components/ExpenseListItem.js
+++ src/Components/ExpenseListItem.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { removeexpense } from '../actions/expenses'
-
+import  moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem=({id,description,amount,createdAt,notes})=>(
     <div>
-        <Link to={`/edit/${id}`}>{description}</Link>
-        <p>Expense Description:{description}</p>
-        <p>CreatedAt:{createdAt}</p>
-        <p>Amount:{amount}</p>
-        <p>Notes:{notes}</p>
+        <Link to={`/edit/${id}`}>
+            <h3>{description}</h3>
+            </Link>
+            <p>
+                {numeral(amount/100).format('$0,0.00')}
+                -
+                {moment(createdAt).format('MMMM Do, YYYY')}
+                </p>
         {/* <button onClick={()=>{dispatch(removeexpense({id}))}}>Remove Expense</button> */}
     </div>
 )
