8b12a926e759e97665fd52803bb49d5caaf1cba8
unknown <swamy.dammala13@gmail.com>
2020-05-30 12:37:32 +0530

    Package.json

--- src/Components/LoadingPage.js
+++ src/Components/LoadingPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const LoadingPage=()=>(
     <div className='loader'>
-    <img className='loader--image' src='images/loader.gif' />
+    {/* //<img className='loader--image' src='images/loader.gif' /> */}
+    <p>Loading...</p>
     </div>
 )
