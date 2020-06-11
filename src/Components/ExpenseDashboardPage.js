04ea540b9551de08a5ca96fc8868ce9af4df5982
unknown <swamy.dammala13@gmail.com>
2020-06-11 21:07:50 +0530

    Added Hidden expenses to dashboard summary

--- src/Components/ExpenseDashboardPage.js
+++ src/Components/ExpenseDashboardPage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilter from './ExpenseListFilter'
 import ExpenseSummary from './ExpenseSummary'
-import DeleteModal from './deleteModal'
 
 const ExpenseDashboardPage=()=>(
     <div>
--- src/Components/ExpenseSummary.js
+++ src/Components/ExpenseSummary.js
@@ -8,10 +8,14 @@ import numeral from 'numeral'
 export const ExpenseSummary=(props)=>{
     const expenseWord=props.expenses.length===1?'expense':'expenses'
     const formattedAmount=numeral(props.expensesTotal/100).format('$0,0.00')
+    console.log(props.expensesWithOutFilters.length)
+    const hiddenItems=props.expensesWithOutFilters.length-props.expenses.length
+    
     return(
         <div className='page-header'>
             <div className='content-container'>
             <h1 className='page-header__title'>Viewing <span>{props.expenses.length}</span> {expenseWord} totalling <span>{formattedAmount}</span></h1>
+            {hiddenItems!==0 && <p>Hidden expenses count <span style={{color:'red'}}>{hiddenItems}</span>.Please remove every filter to see all the items</p>}
             <div className='page-header__actions'>
             <Link className='button' to='/create'>Add Expense</Link>
             </div>
@@ -30,7 +34,8 @@ export const ExpenseSummary=(props)=>{
 const MapStateToProps=(state)=>{
     return {
         expenses:SelectExpenses(state.expenses,state.filters),
-        expensesTotal:ExpenseTotal(SelectExpenses(state.expenses,state.filters))
+        expensesTotal:ExpenseTotal(SelectExpenses(state.expenses,state.filters)),
+        expensesWithOutFilters:SelectExpenses(state.expenses,{text:'',sortBy:'date',startDate:0,endDate:0})
     }
 }
 
